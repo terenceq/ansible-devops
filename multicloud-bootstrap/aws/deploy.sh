772a262e0c245b2d7c1d2b57ace5c93e555b93ae
Vaibhav Kulkarni <Vaibhav.Kulkarni1@ibm.com>
2022-04-18 19:54:26 +0530

    added debug for annotations

--- multicloud-bootstrap/aws/deploy.sh
+++ multicloud-bootstrap/aws/deploy.sh
@@ -301,6 +301,7 @@ fi
 log "==== MAS deployment started ===="
 ## Evalute custom annotations to set with reference from aws-product-codes.config
 # product_code_metadata="$(curl http://169.254.169.254/latest/meta-data/product-codes)"
+set -x
 product_code_metadata="vrxqov1ml7fjwasbi7pkw4m3"
 if [[ -n "$product_code_metadata" ]];then
   log "Product Code: $product_code_metadata"
@@ -327,6 +328,7 @@ if [[ -n "$product_code_metadata" ]];then
 else
   log "MAS product code not found, skipping custom annotations suite_install"
 fi
+set +x
 # Above MAS_HYPERSCALER_* variables are exported for Kind: Suite in suite_install role called from install-suite.yaml
 ansible-playbook mas/install-suite.yml
 log "==== MAS deployment completed ===="
